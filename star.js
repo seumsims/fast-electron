30d109625846a8ace85f71dfeb78e32d0cdbfc4c
seumsims <seumworldinbangladesh@gmail.com>
2022-04-28 07:49:46 +0600

    master fast electron up and running  completed

---
+++ star.js
@@ -0,0 +1,16 @@
+const { BrowserWindow, app } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadFile('index.html')
+  }
+
+  app.whenReady().then(() => {
+    createWindow();
+  })
+
+  
